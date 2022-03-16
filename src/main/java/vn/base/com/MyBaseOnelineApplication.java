3897c5b9d939d1793407d039e97e30a91cedcbdd
Tùng Trần <tungtt09605@gmail.com>
2022-03-17 01:47:03 +0700

    first commit

---
+++ src/main/java/vn/base/com/MyBaseOnelineApplication.java
@@ -0,0 +1,13 @@
+package vn.base.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBaseOnelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBaseOnelineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/base/com/MyBaseOnelineApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.base.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBaseOnelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
